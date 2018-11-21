f490a15b3949be26fff57f76ad7f9dfe0c0c9562
Ryan Waller <rdwaller@gmail.com>
2018-11-20 19:13:45 -0600

    Removed refuse directory

--- refuse/autolex/05-10-18/index.js
+++
@@ -1,9 +0,0 @@
-
-const React = require('react');
-const ReactDOM = require('react-dom');
-const App = require('./components/App');
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
--- refuse/autolex/05-18-18/client/index.js
+++
@@ -1,6 +0,0 @@
-//client/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
